22cd28b57de52299ded8d1e637cf7f3bb5693a2f
luckypaudel <l.paudel@share.epsb.ca>
2025-01-06 19:21:02 -0700

    Worked on folkfest?

--- src/Ex3_MusicFestival/Ex3_Main.java
+++ src/Ex3_MusicFestival/Ex3_Main.java
@@ -26,24 +26,24 @@ public class Ex3_Main {
 
         ((MainStageEvent)allEvents.get(0)).addTech("Pyrotechnics");
         ((MainStageEvent)allEvents.get(0)).addTech("Symphony Orchestra");
-        for (int i = 0; i < allEvents.size(); i++) {
-            if(allEvents.get(i) instanceof MainStageEvent) {
-                System.out.println(allEvents.get(i).getEventName());
-//                ((MainStageEvent) allEvents.get(i)).printTech();
-            }
-        }
+//        for (int i = 0; i < allEvents.size(); i++) {
+//            if(allEvents.get(i) instanceof MainStageEvent) {
+//                System.out.println(allEvents.get(i).getEventName());
+////                ((MainStageEvent) allEvents.get(i)).printTech();
+//            }
+//        }
 
 
 
 while (true) {
-    for (int i = 0; i < allEvents.size(); i++) {
-        if(allEvents.get(i) instanceof MainStageEvent) {
-            System.out.println(allEvents.get(i).getEventName());
-//                ((MainStageEvent) allEvents.get(i)).printTech();
-        }
-    }
+//    for (int i = 0; i < allEvents.size(); i++) {
+//        if(allEvents.get(i) instanceof MainStageEvent) {
+//            System.out.println(allEvents.get(i).getEventName());
+////                ((MainStageEvent) allEvents.get(i)).printTech();
+//        }
+//    }
     System.out.println("Welcome to the FolkFest");
-    System.out.println("Press 1 to Add new event\n Press 2 to Search for an Event\n Press 3 to Filter by event type\n Press 4 to Exit");
+    System.out.println("Press 1 to Add new event\n Press 2 to Search for an Event\n Press 3 to Filter by event type\n Press 4 to print events grouped together \n Press 5 to Add main stage tech  \n Press 6 to exit.");
     int choice = Library.input.nextInt();
     Library.input.nextLine();
     if (choice == 1) {
@@ -94,22 +94,78 @@ while (true) {
             System.out.println("Try again");
         }
     }
-    if(choice == 2){
+    else if(choice == 2){
         System.out.println("What event are you searching for");
         String response = Library.input.nextLine();
         for (int i = 0; i < allEvents.size(); i++) {
-            if(allEvents.equals(response)){
-                System.out.println(allEvents.get(i).getEventName());
+            if(allEvents.get(i).getEventName().equals(response)){
+                Event event = allEvents.get(i);
+                System.out.println("Event Name: " + event.getEventName());
+                System.out.println("Event Type: " + event.getEventType());
+                System.out.println("Location: " + event.getLocation());
+                System.out.println("Time: " + event.getTime());
             }
         }
 
     }
-    if(choice == 3){
-        System.out.println("not yet implemented");
+    else if(choice == 3){
+        System.out.println("Enter type to filter by ");
+        String eventType = Library.input.nextLine();
+        for (Event event : allEvents) {
+            if (eventType.equalsIgnoreCase("MainStageEvent") && event instanceof MainStageEvent) {
+                System.out.println("Event Name: " + event.getEventName());
+            } else if (eventType.equalsIgnoreCase("PopUps") && event instanceof PopUps) {
+                System.out.println("Event Name: " + event.getEventName());
+            } else if (eventType.equalsIgnoreCase("SmallStageEvent") && event instanceof SmallStageEvent) {
+                System.out.println("Event Name: " + event.getEventName());
+            }
+        }
     }
-    if(choice == 4){
-        System.out.println("Thanks!");
+    else if(choice == 4){
+        System.out.println("Print events grouped by type");
+        System.out.println("Non Music events");
+        for (Event event : allEvents) {
+            if (event instanceof NonMusicEvent) {
+                System.out.println("Event name: " + event.getEventName());
+            }
+        }
+            System.out.println("Main Stage events");
+            for (Event event : allEvents) {
+                 if (event instanceof MainStageEvent) {
+                    System.out.println("Event name: " + event.getEventName());
+                }
+            }
+                System.out.println("Small Stage events");
+                for (Event event : allEvents) {
+                    if(event instanceof SmallStageEvent){
+                        System.out.println("Event name: " + event.getEventName());
+                    }
+        }
+    }
+    else if(choice == 5){
+        System.out.println("Search for a main stage event by the name");
+        String name = Library.input.nextLine();
+        boolean found = false;
+        for (Event event : allEvents){
+if(event instanceof MainStageEvent && event.getEventName().equalsIgnoreCase(name) ){
+    found = true;
+    System.out.println("Main event found: " + event.getEventName());
+    String newTech = Library.input.nextLine();
+    ((MainStageEvent)event).addTech(newTech);
+    System.out.println("Tech added succesfully!");
 
+}
+else if(!found){
+    System.out.println("No event was found in Main stage. Try adding it first.");
+    break;
+}
+
+
+        }
+    }
+    else if(choice == 6){
+        System.out.println("Thanks for using the Folkfest Menu!");
+break;
     }
 }
     }
